63263bddc2e393ee58fa6fdb5f3ad6d994756923
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2025-04-15 23:56:36 +0100

    mise à jour de ahp.py

--- ahp.py
+++ ahp.py
@@ -1,25 +1,29 @@
 
 import numpy as np
 
+#Methode qui calcule le poid des critères
 def calculate_weights(comparison_matrix):
-    norm_matrix = comparison_matrix / comparison_matrix.sum(axis=0)
-    weights = norm_matrix.mean(axis=1)
+    norm_matrix = comparison_matrix / comparison_matrix.sum(axis=0) #Normalisation de la matrice
+    weights = norm_matrix.mean(axis=1) #calcul du poids
     return weights
 
+#Methode qui verifie la consistance de la matrice de comparaison
 def consistency_check(comparison_matrix, weights):
-    n = comparison_matrix.shape[0]
-    weighted_sum = comparison_matrix.dot(weights)
-    lambda_max = weighted_sum / weights
-    CI = (np.mean(lambda_max) - n) / (n - 1)
+    n = comparison_matrix.shape[0] # n le nombre de critère
+    weighted_sum = comparison_matrix.dot(weights) # calcul de la somme des poids des critères
+    lambda_max = weighted_sum / weights # calcul de lamda
+    CI = (np.mean(lambda_max) - n) / (n - 1)  # calcule du consistency index
     RI = {1: 0, 2: 0, 3: 0.58, 4: 0.90, 5: 1.12, 6: 1.24,
           7: 1.32, 8: 1.41, 9: 1.45, 10: 1.49}
     
-    CR = CI / RI[n] if n in RI else CI
+    CR = CI / RI[n] if n in RI else CI # calcule du consistency Ration
     return CR
 
+# methode to evaluate the overall weight of each alternative by its criteria weight.
 def evaluate_alternatives(matrix, weights):
     scores = matrix.dot(weights)
     return scores
+
 
 def main(comparison_matrix, alternatives_matrix):
     weights = calculate_weights(comparison_matrix)
@@ -29,7 +33,10 @@ def main(comparison_matrix, alternatives_matrix):
     
     return weights, CR, scores
 
-# Exemple de matrice de comparaison (P, S, Ps, R, M)
+
+###Initialisation des donneées
+
+# Exemple de matrice de comparaison (M, S, C, P, B)
 comparison_matrix = np.array([
     [1, 5, 3, 3, 7],     # M
     [1/5, 1, 1/3, 1/3, 5],   # S
@@ -50,7 +57,7 @@ alternatives = [
 # Créer une matrice avec NumPy
 alternatives_matrix = np.zeros((len(alternatives), len(preferences)))
 
-# Exemple d'assignation de valeurs
+# Exemple d'assignation de valeurs (M, S, C, P, B)
 alternatives_matrix[0] = [4, 64, 3.1, 250000, 3]  # Iphone 12
 alternatives_matrix[1] = [1, 16, 1.3, 40000, 1]  # Itel A56
 alternatives_matrix[2] = [4, 64, 2.0, 60000, 15]  # Tecno Camon 12
@@ -63,7 +70,8 @@ alternatives_matrix[8] = [8, 256, 2.8, 350000, 13]  # Motorola Razr+
 alternatives_matrix[9] = [3, 64, 2.5, 180000, 5]  # Iphone X R
 alternatives_matrix[10] = [8, 256, 2.7, 220000,19 ] # Samsung Galaxy Note 10
 
-# Exécuter le programme
+
+### Exécuter le programme
 weights, CR, scores = main(comparison_matrix, alternatives_matrix)
 
 print("Poids des critères :")
